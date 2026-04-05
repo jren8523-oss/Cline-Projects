63e2479315253a28278a73cc3d219e384cc08399
jren8523-oss <jren8523@gmail.com>
2026-04-05 21:15:07 +0800

    荣誉占位录入

--- honors-data.js
+++ honors-data.js
@@ -1,5 +1,5 @@
 window.honorsData = [
-  { icon: "🏆", title: "全校先进班集体", date: "2025年12月" },
-  { icon: "🏃", title: "校运动会团体总分第一名", date: "2025年10月" },
-  { icon: "📚", title: "优良学风班标兵", date: "2026年03月" }
+  { icon: "🏆", title: "很厉害的班级", date: "2025年12月" },
+  { icon: "🏃", title: "很牛逼的班级", date: "2025年10月" },
+  { icon: "📚", title: "很强大的班级", date: "2026年03月" }
 ];
